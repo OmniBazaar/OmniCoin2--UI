d9a45297ad255d171510a3100000313e7f83262e
Denis Samokhvalov <denis.samokhvalov@scopicsoftware.com>
2018-01-11 22:03:12 +0300

    initial structure and categories localization

---
+++ app/actions/MarketplaceActions.jsx
@@ -0,0 +1,8 @@
+import alt from "alt-instance";
+
+class MarketplaceActions {
+
+}
+
+let MarketplaceActionsWrapped = alt.createActions(MarketplaceActions);
+export default MarketplaceActionsWrapped;
---
+++ app/components/Marketplace/Categories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { connect } from "alt-react";
+import classNames from "classnames";
+var locale_en = require("json-loader!assets/locales/locale-en");
+class Categories extends React.Component {
+
+}
