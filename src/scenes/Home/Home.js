8ea336f378257aebbc883c003bcd1a0eb69636e2
Denis Samokhvalov <denis.samokhvalov@scopicsoftware.com>
2018-02-15 15:44:54 +0300

    initial sidebar

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -3,40 +3,30 @@ import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui
 
 import './home.scss';
 import Burger from './images/hamburger-hover.svg';
+import BackgroundImage from './images/sidebar-bg@2x.jpg';
+import SidebarLogo from './images/logo-sidebar.svg';
 
 
 export default class Home extends Component {
-    state = { visible: false };
+    state = { visible: true };
 
     toggleVisibility = () => this.setState({ visible: !this.state.visible });
 
     render() {
         const { visible } = this.state;
-        let className = visible ? "show-sidebar" : "hide-sidebar";
+        let sideBarClass = visible ? "sidebar visible" : "sidebar";
+        let logoClass = visible ? "" : "hidden";
         return (
-            <div className="home-container">
-                <button onClick={this.toggleVisibility}>Test</button>
-                <Sidebar.Pushable as={Segment}>
-                    <Sidebar  as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
-                        <Menu.Item name='home'>
-                            <Icon name='home' />
-                            Home
-                        </Menu.Item>
-                        <Menu.Item name='gamepad'>
-                            <Icon name='gamepad' />
-                            Games
-                        </Menu.Item>
-                        <Menu.Item name='camera'>
-                            <Icon name='camera' />
-                            Channels
-                        </Menu.Item>
-                    </Sidebar>
-                    <Sidebar.Pusher>
-                        <Segment basic>
 
-                        </Segment>
-                    </Sidebar.Pusher>
-                </Sidebar.Pushable>
+            <div className="home-container">
+                <div className={sideBarClass} style={{backgroundImage: `url(${BackgroundImage})`}}>
+                    <div className="header">
+                        <img className={logoClass} src={SidebarLogo} width={200} height={40}/>
+                        <img src={Burger} width={20} height={20} onClick={this.toggleVisibility}/>
+                    </div>
+                </div>
+                <div className="home-content">
+                </div>
             </div>
         )
     }
