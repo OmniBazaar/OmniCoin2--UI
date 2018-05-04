e048a0ef514e3a9dafefd916484244c22c344845
Denis Samokhvalov <denis.samokhvalov@scopicsoftware.com>
2018-05-04 11:59:13 +0300

    encapsulate processors processing logic

--- app/services/processors/processorsSaga.js
+++ app/services/processors/processorsSaga.js
@@ -23,15 +23,8 @@ function* getTopProcessors() {
   try {
     const globalObject = yield call(getGlobalObject);
     let topProcessors = yield Apis.instance().db_api().exec('get_objects', [globalObject['active_witnesses']]);
-    topProcessors = yield Promise.all(
-      topProcessors.map(el => FetchChain('getAccount', el.witness_account).then(account => ({
-        ...el,
-        witness_account: account.toJS()
-      }))));
-    topProcessors = _.sortBy(topProcessors, ['pop_score'], ['desc']).map((el, index) => ({
-      ...el,
-      rank: index + 1
-    }));
+    topProcessors = yield call(processProcessors, topProcessors);
+    console.log('TOP PROCESSORS ', topProcessors);
     yield put({type: 'GET_TOP_PROCESSORS_SUCCEEDED', topProcessors})
   } catch (error) {
     console.log('ERROR', error);
@@ -48,18 +41,23 @@ function* getStandbyProcessors() {
       .filter(el => !_.includes(topProcessors.map(proc => proc.id), el[1]))
       .map(el => el[1]);
     let standbyProcessors = yield Apis.instance().db_api().exec('get_objects', [standbyProcessorsIds]);
-    standbyProcessors = yield Promise.all(
-      standbyProcessors.map(el => FetchChain('getAccount', el.witness_account).then(account => ({
-        ...el,
-        witness_account: account.toJS()
-      }))));
-    standbyProcessors = _.sortBy(standbyProcessors, ['pop_score'], ['desc']).map((el, index) => ({
-      ...el,
-      rank: index + 1
-    }));
+    standbyProcessors = yield call(processProcessors, standbyProcessors);
     yield put({type: 'GET_STANDBY_PROCESSORS_SUCCEEDED', standbyProcessors});
   } catch (error) {
     console.log('ERROR', error);
     yield put({type: 'GET_STANDBY_PROCESSORS_FAILED', error})
   }
+}
+
+async function processProcessors(processors) {
+  let filteredPr = await Promise.all(
+    processors.map(el => FetchChain('getAccount', el.witness_account).then(account => ({
+      ...el,
+      witness_account: account.toJS()
+    })))
+  );
+  return _.sortBy(filteredPr, ['pop_score'], ['desc']).map((el, index) => ({
+    ...el,
+    rank: index + 1
+  }));
 }
