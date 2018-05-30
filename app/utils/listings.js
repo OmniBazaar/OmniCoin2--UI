a1262cbe48544cd57620ed6146ada2e29849cdfe
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2018-05-29 22:15:34 -0400

    Sends listing items to the API to be saved

--- app/utils/listings.js
+++ app/utils/listings.js
@@ -1,34 +1,31 @@
 import { camelCase } from 'lodash';
 
-export default {
-  getListings(file) {
-    const reader = new FileReader();
+export function getListings(file) {
+  const reader = new FileReader();
+  const onLoadPromise = new Promise((resolve) => {
+    reader.onload = () => {
+      resolve(mapListingsFromFile(reader.result));
+    };
+  });
 
-    const onLoadPromise = new Promise((resolve) => {
-      reader.onload = () => {
-        resolve(this.mapListingsFromFile(reader.result));
-      };
-    });
+  reader.readAsText(file);
 
-    reader.readAsText(file);
+  return onLoadPromise;
+}
 
-    return onLoadPromise;
-  },
+export function mapListingsFromFile(fileContent = '') {
+  const lines = fileContent
+    .split(/\n/)
+    .filter(line => !!line.trim());
+  const header = lines.slice(0, 1)[0].split(/\t/);
+  const content = lines.slice(1);
 
-  mapListingsFromFile(fileContent = '') {
-    const lines = fileContent
-      .split(/\n/)
-      .filter(line => !!line.trim());
-    const header = lines.slice(0, 1)[0].split(/\t/);
-    const content = lines.slice(1);
+  return content.map((line) => {
+    const data = line.split(/\t/);
 
-    return content.map((line) => {
-      const data = line.split(/\t/);
-
-      return data.reduce((object, value, index) => ({
-        ...object,
-        [camelCase(header[index])]: value,
-      }), {});
-    });
-  }
-};
+    return data.reduce((object, value, index) => ({
+      ...object,
+      [camelCase(header[index])]: value,
+    }), {});
+  });
+}
