51ffb4bd9fe86753f0a9b1341aa8d601b78d58e3
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2018-05-31 04:37:02 -0400

    Gets files from amazon

--- app/utils/signed-requester.js
+++ app/utils/signed-requester.js
@@ -1,13 +1,18 @@
 import moment from 'moment';
-import createHmac from 'create-hmac';
+import cryptoJs from 'crypto-js';
 
 import { AWS_ACCESS_KEY, AWS_SECRET_KEY } from './constants';
 
 const REQUEST_METHOD = 'GET';
 const REQUEST_URI = '/onca/xml';
 const ENDPOINT = 'webservices.amazon.com';
-const HMAC_ALGORITHM = 'sha256';
 const TIMESTAMP_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
+
+const toSha256 = (stringToSign, secret) => {
+  const hex = cryptoJs.HmacSHA256(stringToSign, secret);
+
+  return hex.toString(cryptoJs.enc.Base64);
+};
 
 export const canonicalize = (queryObject = {}) => {
   const keys = Object.keys(queryObject);
@@ -19,23 +24,32 @@ export const canonicalize = (queryObject = {}) => {
   return keys.reduce((canonical, key) => {
     const encodedKey = encodeURIComponent(key);
     const encodedValue = encodeURIComponent(queryObject[key]);
+    const prefix = canonical ? `${canonical}&` : '';
 
-    return `${canonical}&${encodedKey}=${encodedValue}`;
+    return `${prefix}${encodedKey}=${encodedValue}`;
   }, '');
 };
 
 export const getSignedUrl = (params) => {
-  const urlParams = {
-    ...params,
-    AWSAccessKeyId: AWS_ACCESS_KEY,
-    Timestamp: `${moment.utc().format(TIMESTAMP_FORMAT)}Z`,
-  };
-  const canonicalQS = canonicalize(urlParams);
+  const AWSAccessKeyId = AWS_ACCESS_KEY;
+  const Timestamp = `${moment.utc().format(TIMESTAMP_FORMAT)}Z`;
+  const newParams = { ...params, AWSAccessKeyId, Timestamp };
+
+  const urlParams = Object.keys(newParams)
+    .sort((a, b) => (a > b) - (a < b))
+    .reduce((acc, key) => ({
+      ...acc,
+      [key]: newParams[key],
+    }), {});
+
+  const canonicalQS = canonicalize(urlParams)
+    .replace('+', '%20')
+    .replace('*', '%2A')
+    .replace('%7E', '~');
   console.log(canonicalQS);
+
   const toSign = `${REQUEST_METHOD}\n${ENDPOINT}\n${REQUEST_URI}\n${canonicalQS}`;
-  const hmac = createHmac(HMAC_ALGORITHM, Buffer.from(AWS_SECRET_KEY))
-    .update(toSign)
-    .digest('hex');
+  const hmac = toSha256(toSign, AWS_SECRET_KEY);
   const sign = encodeURIComponent(hmac);
 
   return `https://${ENDPOINT}${REQUEST_URI}?${canonicalQS}&Signature=${sign}`;
