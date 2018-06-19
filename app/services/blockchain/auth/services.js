e85554893e685520734fe15431feaae686258ffa
Gor <gor.s@scopicsoftware.com>
2018-06-19 10:21:09 +0400

    corrected last login user functionality

--- app/services/blockchain/auth/services.js
+++ app/services/blockchain/auth/services.js
@@ -1,9 +1,11 @@
 const key = 'currentUser';
 
 export const getStoredCurrentUser = () => JSON.parse(localStorage.getItem(key));
+export const getLastStoredUserName = () => JSON.parse(localStorage.getItem('lastLoginUserName'));
 
 export const storeCurrentUser = (currentUser) => {
   localStorage.setItem(key, JSON.stringify(currentUser));
+  localStorage.setItem('lastLoginUserName', JSON.stringify(currentUser.username));
 };
 
 export const removeStoredCurrentUser = () => {
