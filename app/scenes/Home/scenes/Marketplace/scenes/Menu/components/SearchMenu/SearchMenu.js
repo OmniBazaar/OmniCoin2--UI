6fc4f6de9d154c279ef96cf53a0b419fb2588d91
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2018-07-05 00:19:22 -0400

    OM-313 Fixes search menu saved searches

--- app/scenes/Home/scenes/Marketplace/scenes/Menu/components/SearchMenu/SearchMenu.js
+++ app/scenes/Home/scenes/Marketplace/scenes/Menu/components/SearchMenu/SearchMenu.js
@@ -18,6 +18,7 @@ import {
   Icon
 } from 'semantic-ui-react';
 import hash from 'object-hash';
+import dateformat from 'dateformat';
 
 import './search-menu.scss';
 
@@ -119,13 +120,16 @@ class SearchMenu extends Component {
         :
         savedSearches.slice(0, maxSearches).map((search) => (
           <Grid.Row key={hash(search)}>
-            <Grid.Column width={8}>
+            <Grid.Column width={6}>
               <a onClick={() => this.handleSubmit({ search })}>
                 {search.searchTerm}
               </a>
             </Grid.Column>
-            <Grid.Column width={8}>
-              <span className="gray-text">{search.date}</span>
+            <Grid.Column width={4}>
+              <span className="gray-text">{search.category}</span>
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <span className="gray-text">{dateformat(search.date)}</span>
             </Grid.Column>
           </Grid.Row>
         ))
