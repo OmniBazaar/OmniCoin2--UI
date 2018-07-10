caf20af757f5c5e3c468f80c9550006e99d5af4d
Tu Tran <tu.t@scopicsoftware.com>
2018-07-10 17:38:02 +0700

    update notification

---
+++ app/services/updateNotification/updateNotificationActions.js
@@ -0,0 +1,14 @@
+import { createActions } from 'redux-actions';
+
+const {
+	checkUpdate,
+	checkUpdateFinish
+} = createActions({
+	CHECK_UPDATE: () => ({}),
+	CHECK_UPDATE_FINISH: (error, hasUpdate, version, updateLink) => ({ error, hasUpdate, version, updateLink })
+});
+
+export {
+	checkUpdate,
+	checkUpdateFinish
+};
---
+++ app/services/updateNotification/updateNotificationSaga.js
@@ -0,0 +1,61 @@
+import {
+  takeEvery,
+  put,
+  all,
+  call,
+  select
+} from 'redux-saga/effects';
+import { delay } from 'redux-saga';
+import electron from 'electron';
+import request from 'request-promise-native';
+
+import {
+	checkUpdate,
+	checkUpdateFinish
+} from './updateNotificationActions';
+
+export function* updateNotificationSubscriber() {
+  yield all([
+    takeEvery('CHECK_UPDATE', checkUpdateHandler)
+  ]);
+}
+
+const checkInterval = 10000;
+const updateServerUrl = 'http://35.171.116.3/updates';
+
+const isProd = () => {
+  return process.env.NODE_ENV === 'production';
+};
+
+const checkNewVersion = async (version, platform) => {
+	const opts = {
+    uri: `${updateServerUrl}/update/check`,
+    qs: {
+    	version,
+    	platform
+    },
+    method: 'GET'
+  };
+  const body = await request(opts);
+  return JSON.parse(body);
+}
+
+function* checkUpdateHandler() {
+	if (!isProd()) {
+		return;
+	}
+
+	try {
+		const currentVersion = (electron.app || electron.remote.app).getVersion();
+		const platform = process.platform;
+		const { hasUpdate, version, link } = yield checkNewVersion(currentVersion, platform);
+		const updateLink = link ? `${updateServerUrl}/${link}` : '';
+		yield put(checkUpdateFinish(null, hasUpdate, version, updateLink));
+	} catch(err) {
+		console.log('Check update error', err);
+		yield put(checkUpdateFinish(err));
+	} finally {
+		yield delay(checkInterval);
+		yield put(checkUpdate());
+	}
+}
