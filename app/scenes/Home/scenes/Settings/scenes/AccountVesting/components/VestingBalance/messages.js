5bebf7462cab86b2b9ffa1e13ee59291b5945b8c
swoopyy <swoopyyy@gmail.com>
2018-07-11 13:18:41 +0300

    fix vesting none type message

--- app/scenes/Home/scenes/Settings/scenes/AccountVesting/components/VestingBalance/messages.js
+++ app/scenes/Home/scenes/Settings/scenes/AccountVesting/components/VestingBalance/messages.js
@@ -49,7 +49,7 @@ export default defineMessages({
   },
   [VestingTypes.none]: {
     id: 'VestingBalance.none',
-    defaultMessage: ''
+    defaultMessage: '\u200b'
   },
   [VestingTypes.escrowFee]: {
     id: 'VestingBalance.escrowFee',
