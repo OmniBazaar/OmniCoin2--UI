39ee92b7c4d13bc4a799664b0026316e244951e0
Denis Samokhvalov <denis.samokhvalov@scopicsoftware.com>
2018-08-06 15:53:13 +0300

    OM-429 - [vesting balance] remove lines 2 and 3 and change the legend for line 4

--- app/scenes/Home/scenes/Settings/scenes/AccountVesting/components/VestingBalance/VestingBalance.js
+++ app/scenes/Home/scenes/Settings/scenes/AccountVesting/components/VestingBalance/VestingBalance.js
@@ -76,14 +76,14 @@ class VestingBalance extends Component {
               {vb.balance.amount / TOKENS_IN_XOM} XOM
             </Table.Cell>
           </Table.Row>
-          <Table.Row>
-            <Table.Cell>{formatMessage(messages.cdEarned)}</Table.Cell>
-            <Table.Cell textAlign="right">
-              {formatMessage(messages.coinDays, {
-                  number: Math.ceil(earned / secondsPerDay / TOKENS_IN_XOM)
-              })}
-            </Table.Cell>
-          </Table.Row>
+          {/*<Table.Row>*/}
+            {/*<Table.Cell>{formatMessage(messages.cdEarned)}</Table.Cell>*/}
+            {/*<Table.Cell textAlign="right">*/}
+              {/*{formatMessage(messages.coinDays, {*/}
+                  {/*number: Math.ceil(earned / secondsPerDay / TOKENS_IN_XOM)*/}
+              {/*})}*/}
+            {/*</Table.Cell>*/}
+          {/*</Table.Row>*/}
           {/*<Table.Row>*/}
             {/*<Table.Cell>{formatMessage(messages.cdRequired)}</Table.Cell>*/}
             {/*<Table.Cell textAlign="right">*/}
@@ -95,12 +95,12 @@ class VestingBalance extends Component {
               {/*})}*/}
             {/*</Table.Cell>*/}
           {/*</Table.Row>*/}
-          {/*<Table.Row>*/}
-            {/*<Table.Cell>{formatMessage(messages.cdRequired)}</Table.Cell>*/}
-            {/*<Table.Cell textAlign="right">*/}
-              {/*{(availablePercent * 100).toFixed(2)}% / {(availablePercent * vb.balance.amount / TOKENS_IN_XOM).toFixed(2)}*/}
-            {/*</Table.Cell>*/}
-          {/*</Table.Row>*/}
+          <Table.Row>
+            <Table.Cell>{formatMessage(messages.cdRequired)}</Table.Cell>
+            <Table.Cell textAlign="right">
+              {(availablePercent * 100).toFixed(2)}% / {(availablePercent * vb.balance.amount / TOKENS_IN_XOM).toFixed(2)}
+            </Table.Cell>
+          </Table.Row>
           <Table.Row>
             <Table.Cell>{formatMessage(messages.availableToClaim)}</Table.Cell>
             <Table.Cell textAlign="right">
