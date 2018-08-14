a68764fcc38f831a4af3493ecc5e067b88252224
Denis Samokhvalov <denis.samokhvalov@scopicsoftware.com>
2018-08-14 19:43:25 +0300

    OM-454 - [Wallet] Transaction Processor popup appears every time opening wallet page

--- app/scenes/Home/scenes/Settings/scenes/PublicData/PublicData.js
+++ app/scenes/Home/scenes/Settings/scenes/PublicData/PublicData.js
@@ -147,7 +147,7 @@ class PublicData extends Component {
   }
 
   componentWillMount() {
-    const account = this.props.auth.account || {};
+    const { account } = this.props.auth;
     if (account.is_referrer !== this.props.account.referrer) {
       this.toggleReferrer();
     }
@@ -161,7 +161,7 @@ class PublicData extends Component {
       this.props.accountSettingsActions.changeIpAddress(account.publisher_ip);
     }
     if (account.is_a_processor !== this.props.account.transactionProcessor) {
-      //this.toggleTransactionProcessor();
+      this.toggleTransactionProcessor();
     }
     // todo referrer
     this.freezeSettings();
@@ -230,8 +230,7 @@ class PublicData extends Component {
   }
 
   toggleTransactionProcessor() {
-    const account = this.props.auth.account || {};
-    const { is_a_processor } = account;
+    const { is_a_processor } = this.props.auth.account;
     const { transactionProcessor } = this.props.account;
     if (is_a_processor && !transactionProcessor) {
       this.props.accountSettingsActions.setTransactionProcessor(this.state.wantsToVote);
@@ -373,7 +372,7 @@ class PublicData extends Component {
               src={this.getTransactionIcon()}
               width={iconSize}
               height={iconSize}
-              className={cn('checkbox', this.props.auth.account && this.props.auth.account.is_a_processor ? 'disabled' : '')}
+              className={cn('checkbox', this.props.auth.account.is_a_processor ? 'disabled' : '')}
               onClick={this.toggleTransactionProcessor}
             />
           </div>
