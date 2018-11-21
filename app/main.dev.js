af35039c376232a93140c436d4f844e2788173c1
Denis Samokhvalov <denis.samokhvalov@scopicsoftware.com>
2018-11-21 19:01:46 +0200

    fix witness id

--- app/main.dev.js
+++ app/main.dev.js
@@ -164,7 +164,7 @@ const restartNodeIfExists = (witnessId, pubKey, privKey) => {
     if (err) {
       console.log('ERROR ', err);
     } else {
-      const wStart = data.indexOf('#witness-id') !== -1 ? data.indexOf('#witness-id') : data.indexOf('witness-id');
+      const wStart = data.indexOf('# witness-id') !== -1 ? data.indexOf('# witness-id') : data.indexOf('witness-id');
       const wEnd = data.indexOf('\n', wStart);
       data = data.replace(data.substring(wStart, wEnd), `witness-id = "${witnessId}"`);
       const pStart = data.indexOf('#private-key') !== -1 ? data.indexOf('#private-key') : data.indexOf('private-key');
