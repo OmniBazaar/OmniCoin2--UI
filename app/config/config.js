da4b17aa697082f39b847a211924b5be8d8ae147
Kostya9116 <harutyunyankostya@yahoo.com>
2018-12-13 12:13:35 +0400

    Added config.json url into config.js file

--- app/config/config.js
+++ app/config/config.js
@@ -3,5 +3,6 @@ export default {
 	easypostApiKey: "EZAK24e734467ad34a129200a9d509010eb805evYpV8nimjOjRn4HtT6w",
 	exchangeServer: "http://35.171.116.3/exchange/api",
 	mailServer: "http://35.171.116.3/sendmail/",
-	apiURL: 'http://35.171.116.3:5050/api'
+	apiURL: 'http://35.171.116.3:5050/api',
+  configJson: 'http://35.171.116.3/config.json'
 };
