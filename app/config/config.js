479466d19db26ca8c5a62e4506a203b18baaee03
Kostya9116 <harutyunyankostya@yahoo.com>
2018-12-13 14:32:17 +0400

    Added config.json url into config.js file

--- app/config/config.js
+++ app/config/config.js
@@ -1,8 +1,8 @@
 export default {
-  updateServer: "http://update.omnibazaar.com",
-  easypostApiKey: "EZAK24e734467ad34a129200a9d509010eb805evYpV8nimjOjRn4HtT6w",
-  exchangeServer: "http://74.208.211.227/exchange/api",
-  mailServer: "http://74.208.211.227/sendmail/",
-  apiURL: 'http://74.208.211.227:5050/api'
-  configJson: 'https://omnibazaar.com/config.json'
+	updateServer: "http://35.171.116.3/updates",
+	easypostApiKey: "EZAK24e734467ad34a129200a9d509010eb805evYpV8nimjOjRn4HtT6w",
+	exchangeServer: "http://35.171.116.3/exchange/api",
+	mailServer: "http://35.171.116.3/sendmail/",
+	apiURL: 'http://35.171.116.3:5050/api',
+  configJson: 'http://35.171.116.3/config.json'
 };
